38887db7a801fb8ce70955e5f0f724b59453f413
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2020-07-29 10:07:33 -0500

    Need to change the redirect url

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
-from .views import HomePageView
 
 app_name = 'home'
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', TemplateView.as_view(template_name='home/index.html'), name='home'),
 ]
