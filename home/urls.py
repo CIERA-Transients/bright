2c9a04b6182a3131ba49428d8bc0811654bb104f
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2020-10-14 13:10:28 -0500

    Changed the java script table engine so that it was easy to download the data as a CSV file and in general I think this plug in gives us much more control, added more tabs and static html pages

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,7 @@ from django.views.generic import TemplateView
 app_name = 'home'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home/index.html'), name='home'),
+    path('sgrbs', TemplateView.as_view(template_name='home/sgrbs.html'), name='sgrbs'),
+    path('pop-properties', TemplateView.as_view(template_name='home/pop_properties.html'), name='pop_properties'),
+    path('data-products-description', TemplateView.as_view(template_name='home/downloadable_data.html'), name='downloadable_data'),
 ]
